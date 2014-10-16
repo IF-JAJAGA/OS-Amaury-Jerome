2978b751318cc3a9f6f8d4f50cf7e54e28ccde60
Courjault Amaury <acourjault@if501-208-13.insa-lyon.fr>
2014-10-16 18:24:17 +0200

    Début question 4 partie 5

--- kernel.c
+++ kernel.c
@@ -1,39 +1,33 @@
 #include "sched.h"
 #include "hw.h"
 
-#define STACK_SIZE 8
+#include <stdlib.h> 
 
-struct ctx_s ctx_ping;
-struct ctx_s ctx_pong;
-struct ctx_s ctx_init;
 
-void ping()
+void funcA()
 {
-	int cpt = 0;
-	while ( 1 ) {
-		cp/home/acourjault/Téléchargements/ospie-start/Partie 3t++;
-		switch_to(&ctx_pong);
+	int cptA = 0;
+	while ( cptA < 3 ) {
+		cptA ++;
+		ctx_switch();
 	}
 }
-
-void pong()
+void funcB()
 {
-	int cpt = 1;
-	while ( 1 ) {
-		cpt+=2;
-		switch_to(&ctx_ping);
+	int cptB = 1;
+	while ( cptB < 6 ) {
+		cptB += 2 ;
+		ctx_switch();
 	}
 }
-
+//------------------------------------------------------------------------
 int kmain ( void )
 {
 	init_hw();
-	init_ctx(&ctx_ping, ping, STACK_SIZE);
-	init_ctx(&ctx_pong, pong, STACK_SIZE);
-	current_ctx = &ctx_init;
-	switch_to(&ctx_ping);
-	/* 	
-	 * Pas atteignable vues nos 2 fonctions
-	 */
-	return (0);
+	create_process(funcB, NULL, STACK_SIZE);
+	create_process(funcA, NULL, STACK_SIZE);
+	start_sched();
+	ctx_switch();
+	/* Pas atteignable vu nos 2 fonctions */
+	return 0;
 }
--- sched.h
+++ sched.h
@@ -1,22 +1,43 @@
-// STACK_SIZE doit être suffisament grand pour contenir les éléments de contexte,
-// les éventuels paramètres des différentes fonctions ainsi que le code lui-même
-// Puisqu'on y trouve des boucles infinies, on choisit une variable assez grande 
-// pour contenir un bon nombre d'informations et pas trop grande pour pouvoir tourner
-// sur le Raspberry Pi.
-#define STACK_SIZE 512
+#ifndef SCHED_H
+#define SCHED_H
+
+#define STACK_SIZE 1024 
 
 typedef void (*func_t) (void);
 
-struct ctx_s {
-// pointeur de pile
-int sp;
-// adresse de l'instruction en cours
-int lr;
-//fonction associée au contexte
-func_t f_ctx;
+
+/*struct ctx_s{
+	unsigned int pc;
+	unsigned int stackPointer;
+};*/
+
+//struct ctx_s* current_ctx;
+
+enum state{NEW,READY,RUNNING,WAITING,DEAD};
+
+struct pcb_s{
+	unsigned int pid;
+	func_t f;
+	unsigned int pc;
+	unsigned int stackPointer;
+	void *args;
+	enum state etat;
+	struct pcb_s *next;
 };
 
-struct ctx_s* current_ctx;
 
-void __attribute__ ((naked)) switch_to (struct ctx_s* ctx);
+/*void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size);
+void __attribute__ ((naked)) switch_to(struct ctx_s* ctx);*/
+void __attribute__ ((naked)) ctx_switch();
 
+void ctx_switch_from_irq();
+
+void init_pcb(struct pcb_s *pcb, unsigned int pid, func_t f, void* args);
+void start_current_process();
+void elect();
+void start_sched();
+
+void create_process(func_t f, void *args, unsigned int stack_size);
+void __attribute__ ((naked)) ctx_switch();
+
+#endif
